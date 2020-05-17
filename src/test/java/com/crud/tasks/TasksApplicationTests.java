20ffe5e2cd4a97c79aa79f588d29fd9704c72fc8
kamilkowalczyk27 <kamilkowalczyk27@o2.pl>
2020-05-17 18:17:10 +0200

    Addinh Module 19.2

---
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.tasks;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
