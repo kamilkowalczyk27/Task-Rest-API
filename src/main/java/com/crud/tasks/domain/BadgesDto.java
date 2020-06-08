f3017f8afe905b6f596f4235d43ebefd161cb207
kamilkowalczyk27 <kamilkowalczyk27@o2.pl>
2020-06-08 20:51:58 +0200

    Addinh Module 22.4

--- src/main/java/com/crud/tasks/domain/BadgesDto.java
+++ src/main/java/com/crud/tasks/domain/BadgesDto.java
@@ -1,20 +1,20 @@
-package com.crud.tasks.domain;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class BadgesDto {
-
-    @JsonProperty("votes")
-    private int votes;
-
-    @JsonProperty("attachments")
-    private AttachmentsByType attachments;
-}
+//package com.crud.tasks.domain;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//@JsonIgnoreProperties(ignoreUnknown = true)
+//public class BadgesDto {
+//
+//    @JsonProperty("votes")
+//    private int votes;
+//
+//    @JsonProperty("attachments")
+//    private AttachmentsByType attachments;
+//}
