7a1871d37074bf57fccd4993f01b0584bff16a1f
kamilkowalczyk27 <kamilkowalczyk27@o2.pl>
2020-08-05 21:07:20 +0200

    Adding Module 29.1

--- src/test/java/com/crud/tasks/mapper/TaskMapperTestSuite.java
+++ src/test/java/com/crud/tasks/mapper/TaskMapperTestSuite.java
@@ -1,4 +1,58 @@
 package com.crud.tasks.mapper;
 
+import com.crud.tasks.domin.Task;
+import com.crud.tasks.domin.TaskDto;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TaskMapperTestSuite {
+
+    @Autowired
+    private TaskMapper taskMapper;
+
+    @Test
+    public void mapToTaskTest() {
+        //Given
+        TaskDto taskDto = new TaskDto(1L, "title", "content");
+        //When
+        Task task = taskMapper.mapToTask(taskDto);
+        //Then
+        assertEquals(taskDto.getId(), task.getId());
+        assertEquals("title", task.getTitle());
+        assertEquals("content", task.getContent());
+    }
+
+    @Test
+    public void mapToTaskDtoTest() {
+        //Given
+        Task task = new Task(1L, "title", "content");
+        //When
+        TaskDto taskDto = taskMapper.mapToTaskDto(task);
+        //Then
+        assertEquals(task.getId(), taskDto.getId());
+        assertEquals("title", taskDto.getTitle());
+        assertEquals("content", taskDto.getContent());
+    }
+
+    @Test
+    public void mapToTaskDtoListTest() {
+        //Given
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task(1L, "title1", "content1"));
+        tasks.add(new Task(2L, "title2", "content2"));
+        //When
+        List<TaskDto> taskDtos = taskMapper.mapToTaskDtoList(tasks);
+        //Then
+        assertEquals(2, taskDtos.size());
+        assertEquals("title1", tasks.get(0).getTitle());
+        assertEquals("content2", tasks.get(1).getContent());
+    }
 }
